bdd3c5066e134bee67f2dc642fba5cff6adea734
Hari Basnet <harikrishna.basnet@yahoo.com>
2019-03-06 15:05:45 +0200

    this is it

--- domres.js
+++ domres.js
@@ -1,7 +1,48 @@
-const titles = document.getElementsByTagName('h1');
 
-console.log(titles);
+// const titles = document.getElementsByTagName('h1');
 
-for(let i = 0; i < titles.length; i++){
-    console.log(titles[i]);
-}
+
+// console.log(titles);
+
+// for (let i = 0; i < titles.length; i++){
+//     console.log(titles[i]);
+// }
+
+// const firstTitle = document.getElementById('first-title');
+// console.log(firstTitle);
+// const secondTitle = document.getElementById('second-title');
+// console.log(secondTitle);
+// const thirdTitle = document.getElementById('third-title');
+// console.log(thirdTitle);
+
+// const titles = document.querySelectorAll('h1');
+// console.log(titles);
+
+// titles.forEach(title => console.log(title));
+
+// const title = document.querySelector('h1');
+// const firstTitle = document.querySelector('#first-title');
+
+// console.log(firstTitle);
+
+// titles.forEach((title,i) => {
+//     if(i % 2 == 0){
+//     title.style.color = 'green';
+//     }
+// });
+
+
+//creating html element 
+
+const title = document.createElement('h1');
+console.log(title);
+
+//set content to the element 
+title.textContent = 'Title-one';
+document.body.appendChild(title);
+
+const createP = document.createElement('p');
+createP.textContent = 'lorem20';
+
+document.body.appendChild(createP);
+
