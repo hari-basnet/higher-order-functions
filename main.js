43422f56138d472953236f6a0544fe267c5cb4b4
Hari Basnet <harikrishna.basnet@yahoo.com>
2019-03-11 23:56:33 +0200

    new exercises

--- main.js
+++ main.js
@@ -60,3 +60,36 @@ console.log(person.getInfo());
 console.log(person.address.city);
 
 person.job = 'student';
+
+// Exercises 
+
+const countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand'];
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook'];
+const newNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// 1 
+// Explain the difference between forEach, map, filter, and reduce.
+
+// The difference between them is that the foreach method loops through every iterable array and execurtes
+// a provided function to the items of the array 
+// returns undefined otherwise sth is returned by the provided function
+
+// Map
+// map method loops through the array and returns a new modified array of same length 
+
+// filter 
+// filter method loops through the array and checks the given condition and returns a new array 
+// containing elements that passes the condition
+
+// reduce
+// reduce method returns a single value, it could be boolean, object, new array
+// good for addition of the array elements and all
+
+// 2
+// Use forEach to console.log each country in the countries array.
+countries.forEach(function(country){
+    console.log(country);
+});
+
+// 3
+
