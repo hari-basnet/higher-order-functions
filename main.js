737f379323f3a8dd24b492bf5e3ccc3ff31d1485
Hari Basnet <harikrishna.basnet@yahoo.com>
2019-03-12 10:23:17 +0200

    adding more code lines

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
-const itCompanies = [ 'Facebook', 'Google', 'Microsoft','Integrify', 'Apple', 'IBM', 'Oracle', 'Amazon'];
+//const itCompanies = [ 'Facebook', 'Google', 'Microsoft','Integrify', 'Apple', 'IBM', 'Oracle', 'Amazon'];
 
 //let itCompanies = [1,2,3];
 
 //print the array using normal loop
 
-for(let i = 0; i < itCompanies.length; i++){
+// for(let i = 0; i < itCompanies.length; i++){
 
-    console.log(itCompanies[i]);
-}
+//     console.log(itCompanies[i]);
+// }
 
 //reverse the array 
 
-console.log(itCompanies.reverse());
+// console.log(itCompanies.reverse());
 
-for(i = itCompanies.length - 1; i >= 0; --i){
+// for(i = itCompanies.length - 1; i >= 0; --i){
 
-    console.log(itCompanies[i]);
-}
+//     console.log(itCompanies[i]);
+// }
 
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+// var numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 
-var total = numbers.reduce(function(total, number) {
-    return total + number;
-});
+// var total = numbers.reduce(function(total, number) {
+//     return total + number;
+// });
 
-console.log(total);
+// console.log(total);
 
 // function sayHello(){
 //     console.log('hello');
@@ -41,25 +41,25 @@ console.log(total);
 
 // setTimeout(sayGoodBye,3000);
 
-const person = {
+// const person = {
 
-    name : 'Hari',
-    age: 1500,
-    address: {
-        street: 'Pihkalankatu 5',
-        postBox: 20110,
-        city: 'Turku'
-    },
-    getInfo: function(){
-        return `I am ${this.name} and I live in ${this.country}. I am ${this.age} years old.`;
-    }
+//     name : 'Hari',
+//     age: 1500,
+//     address: {
+//         street: 'Pihkalankatu 5',
+//         postBox: 20110,
+//         city: 'Turku'
+//     },
+//     getInfo: function(){
+//         return `I am ${this.name} and I live in ${this.address.city}. I am ${this.age} years old.`;
+//     }
 
-};
+// };
 
-console.log(person.getInfo());
-console.log(person.address.city);
+// console.log(person.getInfo());
+// console.log(person.address.city);
 
-person.job = 'student';
+// person.job = 'student';
 
 // Exercises 
 
@@ -92,4 +92,41 @@ countries.forEach(function(country){
 });
 
 // 3
+// Use forEach to console.log each name in the names array.
 
+names.forEach(function(name){
+    console.log(name);
+});
+
+// 4
+// Use forEach to console.log each number in the numbers array.
+newNumbers.forEach(function (number) {
+    console.log(number);
+});
+
+// 5
+// Use map to create a new array by changing each country to uppercase in the countries array;
+let updatedArray = countries.map(function (country){
+    return country.toUpperCase();
+});
+
+// 6
+// Use map to create a new array by changing each number to square in the numbers array
+let squaredNumbers = newNumbers.map(function (number){
+    return number * number;
+})
+console.log(squaredNumbers);
+
+// 7
+// Use map to change to each name to uppercase in the names array
+let changedNames = names.map(function (name){
+    return name.toUpperCase();
+});
+console.log(changedNames);
+
+// 8
+// Use filter to filter out countries containing land.
+let countryWithLand = countries.filter(function (country){
+    return country.match(/[Ll]and/);
+});
+console.log(countryWithLand);
