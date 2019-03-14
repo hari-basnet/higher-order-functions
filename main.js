4cbd40d03d28aed8f9ba7a1e4382c2cd719ea045
Hari Basnet <harikrishna.basnet@yahoo.com>
2019-03-14 16:31:05 +0200

    new exercise done

--- main.js
+++ main.js
@@ -63,7 +63,7 @@
 
 // Exercises 
 
-const countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand'];
+const countries = ['Estonia', 'Finland', 'Sweden','Uzbekistan', 'Denmark', 'Norway', 'IceLand', 'pakistan'];
 const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook'];
 const newNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
@@ -161,9 +161,9 @@ console.log(countryLengthSix);
 // 11
 // Use filter to filter out country start with 'E';
 let countriesNameE = countries.filter(function(country){
-    return country.indexOf('Finland');
+    return country.indexOf(0) === 'E';
 })
-console.log(countriesNameE); //????????????????????????
+console.log(countriesNameE);
 
 // 12
 // Declare a function called getStringLists which takes an array 
@@ -191,8 +191,88 @@ console.log(reduced);
 // 14
 // Use reduce to concatinate all the countries and to produce this sentence: 
 // Estonia, Finland, Sweden, Denmark, Norway, and IceLand are north European countries
-let concatSentence = countries.reduce((ses, element) => {
+let concatSentence = countries.reduce((acc, val, currentIndex)  => {
+    
 
-    return ses =ses + element+ ' ' ;
 }) 
-console.log(concatSentence);
+// console.log(concatSentence);
+
+
+// 15
+// some 
+
+// every
+
+
+// 16
+// Use some to check if some names' length greater than seven in names array
+const checkNames = names.some((name) => {
+
+    return name.length > 7;
+})
+
+console.log(checkNames);
+
+// 17
+// Use every to check if all the countries contain the word land
+
+const checkLand = countries.every(country => {return country.match(/land/i); });
+
+console.log(checkLand);
+
+
+// 18
+// Explain the difference between find and findIndex.
+// find returns the value of array element  ==> in which the function passed to the array
+// findIndex will return the index of array element ==> in which the function passed to the array
+
+
+// 19
+// Use find to find the first country containing only six letters in the countries array
+const countryWithSixLetters = countries.find(country => {
+
+    return country.length === 6;
+})
+
+console.log(countryWithSixLetters);
+
+// 20
+// Use findIndex to find the position of the first country containing only six letters in the countries array
+const countryWithSixLetters2 = countries.findIndex(country => country.length === 6);
+
+console.log(countryWithSixLetters2);
+
+// 21
+// Declare a function called categorizeCountries which returns an array of countries 
+// which have some common pattern(you find the countries array in this 
+// repository as countries.js(eg 'land', 'ia', 'island','stan')).
+const categorizeCountries = function(array, pattern) {
+
+    let searchQuery = new RegExp(pattern);
+
+    let result = array.filter(country => country.match(searchQuery))
+
+    return result;
+
+}
+console.log(categorizeCountries(countries, /ist/));
+ 
+
+// 22
+//
+
+// 23
+// Declare a getFirstTenCountries function and return an array of ten countries. 
+// Use different functional programming to work on the countries.js array
+
+const getXNoOfCountries = function (array, num){
+
+    let xCountries = array.slice(0,num);
+    return xCountries;
+
+}
+
+console.log(getXNoOfCountries(countries, 5));
+
+// 24
+// Declare a getLastTenCountries function which takes the last ten countries and r
